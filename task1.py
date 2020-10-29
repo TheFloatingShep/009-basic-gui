b8e3a9582c846ff3b362a0d27a9786106f8f63b5
Kenji MacMartin <47537573+TheFloatingShep@users.noreply.github.com>
2020-10-29 12:31:38 -0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -7,3 +7,25 @@ the contents of the first 2 entry boxes. It will
 find the product and display the answer in the
 3rd entry box
 """
+import tkinter as tk
+
+def do():
+    x = entry1.get()
+    y = entry2.get()
+    entry3["text"] = int(x) * int(y)
+
+main = tk.Tk()
+
+entry1 = tk.Entry(width = 25)
+label1 = tk.Label(text = "x")
+entry2 = tk.Entry(width = 25)
+button1 = tk.Button(text = "=", command = do)
+entry3 = tk.Label(width = 40)
+
+entry1.pack(side = "left")
+label1.pack(side = "left")
+entry2.pack(side = "left")
+button1.pack(side = "left")
+entry3.pack(side = "left")
+
+main.mainloop()
