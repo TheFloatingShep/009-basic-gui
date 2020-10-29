934f18df5b53c17a23f9dc6c413447bd2be9286c
Kenji MacMartin <47537573+TheFloatingShep@users.noreply.github.com>
2020-10-29 13:00:05 -0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -12,7 +12,7 @@ import tkinter as tk
 def do():
     x = entry1.get()
     y = entry2.get()
-    entry3["text"] = int(x) * int(y)
+    label3["text"] = int(x) * int(y)
 
 main = tk.Tk()
 
@@ -20,12 +20,12 @@ entry1 = tk.Entry(width = 25)
 label1 = tk.Label(text = "x")
 entry2 = tk.Entry(width = 25)
 button1 = tk.Button(text = "=", command = do)
-entry3 = tk.Label(width = 40)
+label3 = tk.Label(width = 40)
 
 entry1.pack(side = "left")
 label1.pack(side = "left")
 entry2.pack(side = "left")
 button1.pack(side = "left")
-entry3.pack(side = "left")
+label3.pack(side = "left")
 
 main.mainloop()
